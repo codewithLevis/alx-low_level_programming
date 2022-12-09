3fe811f8bc857d0f78d13ed46463d4600cc78b44
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-10 02:38:26 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,9 +2,7 @@
 
 /**
  * main - Use for loop
- * 
  * Description: 'Prints alphabetics both upper and lower '
- * 
  * Return: 0
  */
 int main(void)
@@ -12,15 +10,13 @@ int main(void)
 	char  alphabetic;
 
 	for (alphabetic = 'a'; alphabetic <= 'z'; alphabetic++)
-		
-    {
-        	putchar(alphabetic);
-    }
-	for (alphabetic = 'A'; alphabetic <= 'Z'; alphabetic++)
-	    
-    {
-        	putchar(alphabetic);
-    }
+{
+	putchar(alphabetic);
+}
+	for (alphabetic = 'A'; alphabetic <= 'Z'; alphabetic++) 
+{
+	putchar(alphabetic);
+}
 	putchar('\n');
 	return (0);
 }
