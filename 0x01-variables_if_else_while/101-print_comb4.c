4c2ed3a51a5e53ca5864a8b04c4b81c5f7d07aab
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-10 15:59:59 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,18 +9,16 @@ int main(void)
 int number,i,k;
 for (number = '0'; number <= '9'; number++)
 {
-for (i = '1'; i <= '9'; i++)
+for (i = number+1; i <= '9'; i++)
 {
-    for (k = '2'; k <= '9'; k++)
+    for (k = i+1; k <= '9'; k++)
     {
     putchar(number);
     putchar(i);
     putchar(k);
-    if (k < '9')
-    {
-        putchar(',');
-        putchar(' ');
-    }
+
+    putchar(',');
+    putchar(' ');
 
 }
 }
