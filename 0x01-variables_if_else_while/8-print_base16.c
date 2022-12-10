5aecd75b004fa3ced354cb161ff907f2aef6ceff
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-10 04:25:10 +0300

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - print numbers
+*
+*Description: 'prints all the numbers of base 16 in lowercase'
+*
+* Return: value 0
+*/
+int main(void)
+{
+char hexadecimal;
+
+for (hexadecimal = '0'; hexadecimal <= '9'; hexadecimal++)
+{
+putchar(hexadecimal);
+}
+for (hexadecimal = 'a'; hexadecimal <= 'f'; hexadecimal++)
+{
+putchar(hexadecimal);
+}
+	putchar('\n');
+	return (0);
+}
