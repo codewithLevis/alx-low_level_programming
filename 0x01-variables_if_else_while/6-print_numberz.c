4a047d7e792fcd933919a63becdf4cbb48dc76ab
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-10 03:37:20 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,11 +8,10 @@
  */
 int main(void)
 {
-  int numbers;
+int numbers;
 
-  for (numbers = '0'; numbers <= '9'; numbers++)
-
-  putchar(numbers);
+for (numbers = '0'; numbers <= '9'; numbers++)
+putchar(numbers);
 	putchar('\n');
 	return (0);
 }
