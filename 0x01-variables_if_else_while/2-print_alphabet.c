e6002907549fcc0d1037b8a5d061e143d3215ed8
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-10 18:37:03 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,14 +4,14 @@
 *
 *Description:outputs_the alphabets using putchar_func
 *
-* Return: value 0 
+* Return: value 0
 */
 int main(void)
 {
 	char alphabetic = 'a';
-	
+
 	for (alphabetic = 'a'; alphabetic <= 'z'; alphabetic++)
-		
+
 		putchar(alphabetic);
 	putchar('\n');
 	return (0);
