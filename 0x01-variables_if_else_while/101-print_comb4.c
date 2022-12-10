1ba18fe53008dbb89071d7d4bcbe2a92cc1f0559
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-10 16:11:27 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 /**
- * main - prints a numbers separated by comma
- *
- *Return: 0
- */
+* main - prints a numbers separated by comma
+*
+*Return: 0
+*/
 int main(void)
 {
-int number,i,k;
+int number,i,j;
 for (number = '0'; number <= '9'; number++)
 {
 for (i = number+1; i <= '9'; i++)
 {
-    for (k = i+1; k <= '9'; k++)
-    {
-    putchar(number);
-    putchar(i);
-    putchar(k);
+for (j = number+2; j <= '9'; j++)
+{
+putchar((number % 10) + '0');
+putchar((i % 10) + '0');
+putchar((k % 10) + '0');
+if (number == 7 && i == 8 && j == 9)
+continue;
 
-    putchar(',');
-    putchar(' ');
+putchar(',');
+putchar(' ');
 
 }
 }
 
 }
 putchar('\n');
-
+return (0);
 }
