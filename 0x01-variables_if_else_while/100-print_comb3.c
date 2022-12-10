85acc2ad582d9dc8e86401fb74998b64a78bdbf8
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-10 15:54:28 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,20 +6,20 @@
  */
 int main(void)
 {
-int number,i;
+int number, i;
 for (number = 0; number <= 8; number++)
 {
-for (i = number+1 ; i <= 9; i++)
-    {
-    putchar((number % 10) + '0');
-    putchar((i % 10) + '0');
-    if (number ==8 && i ==9 )
-        continue;
+for (i = number + 1 ; i <= 9; i++)
+{
+putchar((number % 10) + '0');
+putchar((i % 10) + '0');
+if (number == 8 && i == 9)
+continue;
 
-    putchar(',');
-    putchar(' ');
+putchar(',');
+putchar(' ');
 
-    }
+}
 
 
 }
