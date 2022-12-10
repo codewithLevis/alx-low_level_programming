3272a7ff85374c002813491561af9ec36e93a5c8
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-10 13:43:51 +0300

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints a numbers separated by comma
+ *
+ *Return: 0
+ */
+int main(void)
+{
+int number;
+for (number = '0'; number <= '9'; number++)
+{
+    putchar(number);
+    if (number <'9')
+{
+        putchar(',');
+        putchar(' ');
+}
+
+
+}
+    putchar('\n');
+
+}
