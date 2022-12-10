d5b5ba587b1bab2368c64bb3e5166b12c72fac3f
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-10 13:06:35 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <unistd.h>
 /**
 * main - write program
 *
@@ -11,4 +12,3 @@ int main(void)
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-//#include <unistd.h>
