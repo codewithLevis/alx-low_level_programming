b725e20cac0a426d43b46e3bcb111e016a3c4995
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-10 13:51:45 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,13 @@ int main(void)
 int number;
 for (number = '0'; number <= '9'; number++)
 {
-    putchar(number);
-    if (number <'9')
+putchar(number);
+if (number < '9')
 {
-        putchar(',');
-        putchar(' ');
+    putchar(',');
+    putchar(' ');
 }
 }
-    putchar('\n');
-    return (0);
+putchar('\n');
+return (0);
 }
