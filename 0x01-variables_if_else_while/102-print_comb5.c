8f019f63620c9bca6dd9e729d6e7386c3af2ddc4
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-10 18:26:12 +0300

    Create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+*main -  prints all possible combinations of two two-digit numbers
+*Return: returns 0
+*/
+int main(void)
+{
+int value1, value2;
+
+for (value1 = 0; value1 <= 98; value1++)
+{
+for (value2 = value1 + 1; value2<= 99; value2++)
+{
+putchar((value1 / 10) + '0');
+putchar((value1 % 10) + '0');
+putchar(' ');
+putchar((value2 / 10) + '0');
+putchar((value2 % 10) + '0');
+
+if (value1 / 10 != 9 || value1 % 10 != 8)
+{
+putchar(' ');
+putchar(',');
+}
+}
+}
+putchar(10);
+return (0);
+}
