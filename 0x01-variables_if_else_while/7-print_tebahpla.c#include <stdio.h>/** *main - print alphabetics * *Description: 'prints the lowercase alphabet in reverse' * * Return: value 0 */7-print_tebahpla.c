b6d3d60871bdcfe2731bff99c7e357694195a64c
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-10 03:53:07 +0300

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c#include <stdio.h>/** *main - print alphabetics * *Description: 'prints the lowercase alphabet in reverse' * * Return: value 0 */7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - print alphabetics
+*
+*Description: 'prints the lowercase alphabet in reverse'
+*
+* Return: value 0
+*/
+int main(void)
+{
+char alphabetic;
+
+for (alphabetic = 'z'; alphabetic >= 'a'; alphabetic--)
+{
+  putchar(alphabetic);
+}
+  putchar('\n');
+  return (0);
+}
