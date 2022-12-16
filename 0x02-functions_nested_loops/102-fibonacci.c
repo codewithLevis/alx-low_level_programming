d63b86b0c5c8cfa97e73112359f5c643f0d142af
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-16 04:24:48 +0300

    Create 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+*main - prints fibonacci numbers
+*Return: 0
+*/
+
+int main(void) {
+int i;
+long unsigned int prev = 1, curr = 2, next;
+
+printf("%lu ,", prev);
+printf("%lu ,", curr);
+i = 2;
+while (i < 49)
+{
+next = prev + curr;
+if(i< 48)
+printf("%lu ,", next);
+if (i == 48)
+printf("%lu", next);
+prev = curr;
+curr = next;
+i++;
+}
+putchar('\n');
+return 0;
+}
