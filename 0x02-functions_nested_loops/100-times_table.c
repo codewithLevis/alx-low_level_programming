52a3ff63188de5d5028c08224dfc660bb0e0b2e9
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-16 03:57:54 +0300

    Create 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_times_table - outputs time table
+ *
+ *@n: provides limit
+ *
+ * Return: 0
+ */
+void print_times_table(int n)
+{
+int num1, num2, product;
+
+if (n <= 15 && n >= 0)
+{
+num1 = 0;
+while (num1 <= n)
+{
+putchar('0');
+num2 = 1;
+while (num2 <= n)
+{
+putchar(',');
+putchar(' ');
+product = num1 *num2;
+if (product < 100)
+{
+putchar(' ');
+}
+if (product <= 9)
+{
+putchar(' ');
+}
+if (product >= 100)
+{
+putchar((product / 100) + '0');
+putchar((product / 10) % 10 + '0');
+}
+else if (product <= 99 && product >= 10)
+{
+putchar((product / 10) + '0');
+}
+putchar((product % 10) + '0');
+num2++;
+}
+putchar('\n');
+num1++;
+}
+}
+}
