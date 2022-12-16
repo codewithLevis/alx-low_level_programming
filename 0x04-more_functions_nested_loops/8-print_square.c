b42bae993d58be0c223819057aeffe104a483e49
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-16 23:18:10 +0300

    Create 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_square - prints a square with # symbol
+* @size - parameter for input
+* Return: 0
+*/
+void print_square(int size)
+{
+if (size > 0)
+{
+int j = 0;
+
+for (; j < size; j++)
+{
+int i = 0;
+for (; i < size; i++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else if (size <= 0)
+{
+_putchar('\n');
+}
+ return (0);
+}
