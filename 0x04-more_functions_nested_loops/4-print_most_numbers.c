68f26383be020b3018ae92a7cbc00c8fc8ca62fa
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-17 03:19:30 +0300

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers -outputs most digits
+ * Return: most digits
+ */
+void print_most_numbers(void)
+{
+int num = '0';
+
+for (;num <= '9'; num++;)
+{
+if  (num != '2' &&  num != '4')
+{
+_putchar(num);
+}
+}
+_putchar('\n');
+}
