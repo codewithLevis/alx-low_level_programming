c59c004fefed3235e22b2cfe5384ba17479d3c7e
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-17 03:06:07 +0300

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - function for numbers  btw 0 and 9
+*
+* Return: numbers
+*/
+void print_numbers(void)
+{
+int nums = '0';
+
+for (;numbers <= '9'; num++;)
+{
+_putchar(num);
+}
+_putchar('\n');
+}
