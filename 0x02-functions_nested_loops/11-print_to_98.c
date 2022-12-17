db163d383fc0e4fd134ea9d0622a3f5f440e8cff
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-17 16:04:37 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 {
 if (n <= 98)
 {
-for (; n<=98; n++)
+for (; n <= 98; n++)
 {
 printf("%d", n);
 if (n < 98)
@@ -29,9 +29,12 @@ for (; n >= 98; n--)
 {
 printf("%d", n);
 if (n > 98)
-
+{
 putchar(',');
 putchar(' ');
+}
+else if (n == 98)
+break;
 }
 putchar('\n');
 }
