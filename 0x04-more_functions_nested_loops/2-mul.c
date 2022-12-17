ea1e16a5a29fe39ad5b43805a7af25166a97a2d3
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-17 03:02:43 +0300

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - gives the product
+ * @a:parameter
+ * @b:pararmeter
+ * Return: a*b
+ */
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	return (product);
+}
