759ed19f716061e85720a82149f0eb6e6831d2e0
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-17 16:16:52 +0300

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - finds total of all multiples of 3 and 5 between 0 and 1024
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int mult = 0;
+	int tot= 0;
+
+	for (;mult <= 1024; mult++)
+	{
+		if (mult > 0)
+		{
+			if (mult % 5 == 0 || mult % 3 == 0)
+			{
+				tot = mult + tot;
+			}
+		}
+	}
+	printf("%d", tot);
+	printf("\n");
+	return (0);
+}
