b5b0ce371f56dc66226b1fde3fe3024495ceaf8e
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-21 01:52:45 +0300

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * _strlen -returns the length of a string
+ *
+ * @s: pointer parameter
+ *
+ * Return: length of string
+ *
+ */
+
+int _strlen(char *s)
+{
+	int strlen;
+
+	strlen = 0;
+	while (*s != '\0')
+    {
+        ++strlen;
+        s++;
+    }
+
+	return (strlen);
+}
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: parameter
+ *
+ * Return: non-value
+ */
+
+void puts_half(char *str)
+{
+int x;
+int ext = _strlen(str);
+
+if (ext % 2 == 0)
+{
+x = ext / 2;
+
+for (; str[x] != '\0'; x++)
+putchar(str[x]);
+
+}
+else if (ext % 2 != 0)
+{
+x = (ext / 2) + 1;
+
+for (; str[x] != '\0'; x++)
+putchar(str[x]);
+
+}
+putchar('\n');
+}
