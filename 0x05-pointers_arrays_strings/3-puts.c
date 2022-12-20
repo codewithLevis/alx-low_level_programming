6c1d8aa1cc85ef897eb82c2cca120b36f9b97691
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-21 00:55:00 +0300

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+  #include "main.h"
+  /**
+  * _puts - Prints a string
+  *
+  * @str: parameter
+  *Return: non-value
+  */
+  void _puts(char *str)
+  {
+  int call;
+
+  while (str[call] != '\0')
+  {
+  _putchar(str[call]);
+  ++call;
+  }
+  _putchar('\n');
+  }
