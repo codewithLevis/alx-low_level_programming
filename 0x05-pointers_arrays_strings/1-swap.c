c81344630a12bbb4afb591111facf8a4f8cd0aa2
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-21 00:32:05 +0300

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* swap_int -  swaps the values of two integers
+*
+* @a: first swapping parameter
+* @b: second swapping parameter
+*
+* Return: non-value
+*/
+
+void swap_int(int *a, int *b)
+{
+int shift;
+
+swp = *a;
+*a = *b;
+*b = shift;
+}
