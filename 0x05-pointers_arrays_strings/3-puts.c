248591bf4ab835c5c3b1840619a501593c7aa4c3
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-21 03:47:54 +0300

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,18 @@
-  #include "main.h"
-  /**
-  * _puts - Prints a string
-  *
-  * @str: parameter
-  *Return: non-value
-  */
-  void _puts(char *str)
-  {
-  int call;
+#include "main.h"
+/**
+* _puts - Prints a string
+*
+* @str: parameter
+*Return: non-value
+*/
+void _puts(char *str)
+{
+int call;
 
-  while (str[call] != '\0')
-  {
-  _putchar(str[call]);
-  ++call;
-  }
-  _putchar('\n');
-  }
+while (str[call] != '\0')
+{
+_putchar(str[call]);
+call++;
+}
+_putchar('\n');
+}
