0867500bc6a88b566b9f011888f92f78f7d339e6
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-21 04:07:51 +0300

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,25 +1,25 @@
-    #include "main.h"
+#include "main.h"
 
-    /**
-    * print_array - prints array of intergers
-    *
-    * @a: pointer parameter
-    * @n: Calling parameter
-    *
-    * Return: no value
-    */
-    void print_array(int *a, int n)
-    {
-    int x;
+/**
+* print_array - prints array of intergers
+*
+* @a: pointer parameter
+* @n: Calling parameter
+*
+* Return: no value
+*/
+void print_array(int *a, int n)
+{
+int x;
 
-    for (; x < n; x++)
-    {
-    if (x <= n - 2)
-    printf("%d, ", a[x]);
-    else if (x == n - 1)
-    printf("%d", a[x]);
+for (; x < n; x++)
+{
+if (x <= n - 2)
+printf("%d, ", a[x]);
+else if (x == n - 1)
+printf("%d", a[x]);
 
-    }
+}
 
-    printf("\n");
-    }
+printf("\n");
+}
