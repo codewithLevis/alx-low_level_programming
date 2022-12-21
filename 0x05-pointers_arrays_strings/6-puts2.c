a608286f47548bb1ec2ccf05594e6b834eb21781
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-21 04:04:32 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,18 @@
-  #include "main.h"
+#include "main.h"
 
-  /**
-  * puts2 - prints every other character of a string
-  *
-  * @str: Swapping Parameter
-  *
-  * Return: non
-  */
-  void puts2(char *str)
-  {
-  int i = 0;
+/**
+* puts2 - prints every other character of a string
+*
+* @str: Swapping Parameter
+*
+* Return: non
+*/
+void puts2(char *str)
+{
+int i = 0;
 
-  for (; str[i] != '\0'; i++)
-  if (i % 2 != 1)
-  putchar(str[i]);
-  putchar('\n');
-  }
+for (; str[i] != '\0'; i++)
+if (i % 2 != 1)
+putchar(str[i]);
+putchar('\n');
+}
