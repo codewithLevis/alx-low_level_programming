0c0a2d3252ced2550462e312cda8f87a57d5072f
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-23 01:43:26 +0300

    Create 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strncat  - concatinates strings
+*
+* @dest: first pointer argument
+* @src: second pointer argument
+* @n: parameter for extent of string
+*
+* Return: copied string
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int m = 0;
+
+do {
+dest[m] = src[m];
+m++;
+} while (m < n && src[m] != '\0');
+for ( ; m < n; m++)
+dest[m] = '\0';
+
+return (dest);
+}
