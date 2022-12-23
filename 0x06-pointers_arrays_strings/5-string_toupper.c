751bc01cca6072ebb7bbdd9c7ad4b239d35946bd
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-23 08:12:14 +0300

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*string_toupper - changes all lowercase letters of a string to uppercase
+*@str: parameter
+*Return: 0
+*/
+
+char *string_toupper(char *str)
+{
+int v = 0;
+
+do {
+if (str[v] >= 97 && str[v] <= 122)
+str[v] -= 32;
+} while (str[v++]);
+
+return (0);
+}
