6ff285cc0a3bfb615facdf5182e499882c516542
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-24 00:19:19 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,31 +1,26 @@
 #include "main.h"
 /**
 * leet - encodes a string into 1337
-*@str: parameter
-*Return: str
+*@unit: parameter
+*Return: unit
 */
 
-char *leet(char *str)
-
+char *leet(char *unit)
 {
-int v = 0;
+char value[8] = {'O', 'L', '!', 'E', 'A', '!', '!', 'T'};
+
+int range1, range2;
+range1 = 0;
 
 
 do {
-if ((str[v] >= 97 && str[v] <= 122) || (str[v] >= 65 && str [v] <= 90))
+range2 = 0;
+while (range2 <= 7)
 {
-if(str[v] == 97 || str[v] == 65)
-str[v] = '4';
-if(str[v] == 101 || str[v] == 69)
-str[v] = '3';
-if(str[v] == 111 || str[v] == 79)
-str[v] = '0';
-if(str[v] == 116 || str[v] == 84)
-str[v] = '7';
-if(str[v] == 108 || str[v] == 76)
-str[v] = '1';
+if (unit[range1] == value[range2] || unit[range1] - 32 == value[range2])
+unit[range1] = range2 + '0';
+range2++;
 }
-} while (str[v++]);
-
-return (str);
+} while (unit[++range1]);
+return (unit);
 }
