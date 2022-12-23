1a2570ed16e8697fd0f7988b823b5258f94db11e
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-24 02:27:11 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,14 +12,16 @@ char *rot13(char *unit)
 int letter;
 
 do {
-for (;(unit[letter] >= 'a' && unit[letter] <= 'z') || (unit[letter] >= 'A' && unit[letter] <= 'Z'); letter++)
+for (; (unit[letter] >= 'a' && unit[letter] <= 'z') 
+|| (unit[letter] >= 'A' && unit[letter] <= 'Z'); letter++)
 {
-if ((unit[letter] >= 'A' && unit[letter] <= 'M') || (unit[letter] >= 'a' && unit[letter] <= 'm'))
+if ((unit[letter] >= 'A' && unit[letter] <= 'M')
+|| (unit[letter] >= 'a' && unit[letter] <= 'm'))
 unit[letter] += 13;
 else
 unit[letter] -= 13;
 }
 letter++;
-}while (unit[letter] != '\0');
+} while (unit[letter] != '\0');
 return (unit);
 }
