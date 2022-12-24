36123d0d7188a1bfdc78d3e8d43ec4b9f942d692
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-24 17:21:37 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,21 +5,49 @@
 *Return: 0
 */
 
-int main(void)
-{
+int main(void) {
 int i;
-unsigned long int prev = 0, curr = 1, next;
+unsigned long n1, n2, n3, n4;
+unsigned long h1, h2;
+unsigned long prev = 0, curr = 1, next;
 
-for (i = 2; i <= 99; i++)
+i = 0;
+while (i < 92)
 {
 next = prev + curr;
-if (i <= 98)
+if (i < 97)
 printf("%lu, ", next);
-if (i == 99)
+if (i == 97)
 printf("%lu", next);
 prev = curr;
 curr = next;
+i++;
+}
+i = 93;
+n1 = prev / 10000000000;
+n3 = curr / 10000000000;
+n2 = prev % 10000000000;
+n4 = curr % 10000000000;
+while (i < 99)
+{
+
+h1 = n1 + n3;
+h2 = n2 + n4;
+if (n2 + n4 > 9999999999)
+{
+h1 = h1 + 1;
+h2 = h2 % 10000000000;
+}
+printf("%lu%lu", h1, h2);
+if (i != 98)
+printf(", ");
+n1 = n3;
+n2 = n4;
+n3 = h1;
+n4 = h2;
+i++;
 }
 
+printf("\n");
 return 0;
 }
