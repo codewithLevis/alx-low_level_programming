41e6d5a5b913f677683664ca86f021f490bda13a
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-24 16:32:24 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+/**
+*main - prints fibonnaci numbers
+*
+*Return: 0
+*/
 
-int main(void) {
+int main(void)
+{
 int i;
 unsigned long int prev = 0, curr = 1, next;
 
-for (i = 2; i < 98; i++)
+for (i = 2; i <= 99; i++)
 {
 next = prev + curr;
-if (i < 97)
+if (i <= 98)
 printf("%lu, ", next);
-if (i == 97)
+if (i == 99)
 printf("%lu", next);
 prev = curr;
 curr = next;
