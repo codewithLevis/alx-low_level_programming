5ed2917983a70282aad94feecf3748497410693a
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2022-12-27 20:40:37 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,12 +6,13 @@
 *Return: 0;
 */
 
-int main(void) {
+int main(void)
+{
 int64_t num = 612852475143;
 int64_t largest_factor = 0;
 int64_t a = 2;
 
-while(a <= sqrt(num))
+while (a <= sqrt(num))
 {
 for (;num % a == 0;)
 {
@@ -21,11 +22,10 @@ num /= a;
 a++;
 }
 
-if (num > 1) {
+if (num > 1)
 largest_factor = num;
-}
 
-printf("%d\n", largest_factor);
+printf("%ld\n", largest_factor);
 
-return 0;
+return (0);
 }
