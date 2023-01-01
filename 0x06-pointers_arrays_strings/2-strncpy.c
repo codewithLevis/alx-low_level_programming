10dfc14d2da1b5b7019b4af70ee0cb8eb64ae663
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-01 20:16:03 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,8 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
-for( src[i] != '\0'; i < n ; i++)
-{
+for (; i < n; i++)
 dest[i] = src[i];
-
-}
 for (; i < n; i++)
 dest[i] = '\0';
 return (dest);
