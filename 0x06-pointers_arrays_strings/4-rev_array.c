c8b552a2d2f039f21d62c70ee17820b35bce36e3
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-02 08:32:16 +0300

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*reverse_array - reverses the content of an array of integers
+*@a: pointer to the passed parameter
+*@n: sizeof(a) / sizeof(int)
+*Return: non-value
+*/
+void reverse_array(int *a, int n)
+{
+int control = 0, rev;
+
+for (n = n - 1;control < n ;)
+{
+rev = a[control];
+a[control++] = a[n];
+a[n--] = rev;
+}
+
+}
