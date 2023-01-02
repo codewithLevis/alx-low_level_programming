db700b957181fb8637a354630eaa8aa2af2fc104
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-02 08:59:48 +0300

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*cap_string -  capitalizes all words of a string.
+*@value: parameter for passing string
+*Return: value
+*/
+char *cap_string(char *value)
+{
+int i = 0;
+for(; value[i] != '\0'; i++)
+{
+
+if (value[i] == ' ' || value[i] == '\t' || value[i] == '\n'
+|| value[i] == ',' || value[i] == ';' || value[i] == '.'
+|| value[i] == '.' || value[i] == '!' || value[i] == '?'
+|| value[i] == '"' || value[i] == '(' || value[i] == ')'
+|| value[i] == '{' || value[i] == '}')
+{
+if (value[i + 1] >= 'a' && value[i + 1] <= 'z')
+value[i + 1] -= 32;
+}
+
+}
+return(value);
+}
