9f6a975f682d0b6b2c584aded115eaeb0a71408f
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-04 01:40:08 +0300

    Create 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*print_chessboard -  prints the chessboard
+*
+*@a: pointer to an array character
+*Return: non-value
+*/
+void print_chessboard(char (*a)[8])
+{
+int x, y;
+
+x = 0;
+
+do {
+y = 0;
+do{
+putchar(a[x][y]);
+y++;
+} while (y < 8);
+putchar('\n');
+x++;
+
+} while (x < 8);
+}
