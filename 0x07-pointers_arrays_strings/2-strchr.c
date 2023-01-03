649f36fc98fd295390274ad07daf2e010ecca8d5
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-04 00:36:02 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,13 +4,13 @@
 *
 *@s:pointer to string
 *@n: parameter to chracter to be passed
-*Return: a pointer to the first occurrence of the character c in the string s, or NULL
+*Return: a pointer to the first occurrence of the character c in the string s
 */
 char *_strchr(char *s, char c)
 {
 int x;
 
-for (x = 0;s[x] != '\0' && s[x] != c; x++)
+for (x = 0; s[x] != '\0' && s[x] != c; x++)
 ;
 if (s[x] == c)
 return (&s[x]);
