64e917fefe2f2afec1b064bc315f8dc3b944071e
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-06 02:53:20 +0300

    Create 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_pow_recursion - outputs power of a number
+*
+*@x: powered number parameter
+*@y: powering number parameter
+*
+*Returns: the power, -1 or 1
+*/
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 0)
+return (1);
+
+return ( x * _pow_recursion(x, y - 1));
+}
