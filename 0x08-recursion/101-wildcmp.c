9d9024cf6fcb2a84666c76d2752f51012274bfba
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-07 10:35:39 +0300

    Create 101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* wildcmp - function that compares two strings
+*The special char * can replace any string
+*
+*@s1: pointer to string 1
+*@s2:pointer to string 2
+*
+*Return: returns 1 if the strings areidentical, otherwise 0
+*/
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+
+if ((*s1 != '\0' && *s2 == '\0'))
+return (0);
+
+if (*s2 == '*')
+return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+
+if (*s1 != *s2)
+return (0);
+
+return (wildcmp(s1 + 1, s2 + 1));
+}
