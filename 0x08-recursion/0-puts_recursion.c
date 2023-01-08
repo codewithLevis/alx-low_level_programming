f5c161a4899fb7319be98cffb64639cf8984cb98
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-08 08:16:54 +0300

    Create 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*_puts_recursion - prints a string, followed by a new line
+*
+*@s: string pointer
+*
+*Return:non-value
+*/
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+putchar ('\n');
+return (0);
+}
+
+if (*s != '\0')
+putchar (*s);
+
+_puts_recursion(s + 1);
+
+}
