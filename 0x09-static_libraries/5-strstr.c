10b267903ccc65a9b70355b17c6d6845507bb459
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-11 02:19:17 +0300

    Create 5-strstr.c

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+*_strstr - finds occurrence of the substring needle in the string haystack
+*
+*@haystack: first string pointer
+*@needle: second string pointer
+*
+*Return: a pointer to the beginning of the located substring or NULL
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+int x, y;
+
+x = 0;
+
+do {
+y = 0;
+while (needle[y] != '\0')
+{
+if (haystack[x + y] != needle[y])
+break;
+y++;
+}
+
+if (!needle[y])
+return (&haystack[x]);
+x++;
+} while (haystack[x] != '\0');
+return (NULL);
+}
