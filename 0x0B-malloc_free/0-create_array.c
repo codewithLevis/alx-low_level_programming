3fffbca71e30832ae004e1a74cd87e2543d54030
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-12 19:27:34 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,7 +4,7 @@
 *create_array - creates an array of chars
 *And initializes it with a specific char
 *@size: number of characters
-@c: passed character
+*@c: passed character
 *Return: Null or 0
 */
 char *create_array(unsigned int size, char c)
@@ -13,14 +13,14 @@ unsigned int i;
 char *ptr;
 
 if (size == 0)
-return(0);
+return (0);
 ptr = malloc(sizeof(char) * size);
 
 if (ptr == 0)
 return (0);
-for(i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
 ptr[i] = c;
 }
-return(ptr);
+return (ptr);
 }
