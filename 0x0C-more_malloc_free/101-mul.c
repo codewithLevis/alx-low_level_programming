84ccad3f2236f110deac2b1404709e97645ae6f5
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-14 00:30:23 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -126,39 +126,12 @@ int stringlen(char *s)
 
 	len = 0;
 	
-	while (*s !- '\0')
+	while (*s != '\0')
 		s++;
 
 	return (len);
 }
-{
-int x =0, y = 0, z = 0, ext = 0, result = 0;
-int sign;
-while (s[ext] != '0')
-ext++;
-for (; x < ext && y == 0; x++)
-{
-if (s[x] == '-')
-z++;
-if (s[x] >= '0' && s[x] <= '9')
-{
-sign = s[x] - '0';
-if (z % 2)
-sign = -sign;
-result = result * 10 + sign;
-y = 1;
-if (s[x + 1] < 48 || s[x + 1] > 57)
-break;
-y = 0;
 
-}
-}
-
-if (y == 0)
-return (0);
-return (result);
-}
-*/
 /**
 *mul -  multiplies two positive numbers
 *@num1: pointer to first positive number
