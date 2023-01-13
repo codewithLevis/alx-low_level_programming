088594a319f0d9958eee47356f145b674381a429
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-14 00:19:02 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,38 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 
-int _isnum(const char *str) {
-    int i = 0;
+int conf_digit(int c);
+int conf_number(char *num);
+int conv_to_int(char *s);
+int mul(char *num1, char *num2);
 
-    while (str[i]) {
-        if (!isdigit(str[i])) {
-            return 0;
-        }
-        i++;
-    }
+/**
+*main - final execution of code
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: 0
+*/
 
-    return 1;
+int main(int argc, char *argv[])
+{
+	int product;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+
+	if (!conf_number(argv[1]) || !conf_number(argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	product = mul(argv[1], argv[2]);
+	printf("%d\n", product);
+    	return (0);
 }
 
-int _mul(const char *n1, const char *n2) {
-    return atoi(n1) * atoi(n2);
+/**
+*  conf_digit -confirms if digits
+*  @c: parameter for passing argument
+*  Return: 0 or 1
+*/
+int conf_digit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+	return (1);
+	}
+
+	else
+	{
+	return (0);
+	}
 }
 
-int main(int argc, char *argv[]) {
-    int result;
-    if (argc != 3) {
-        printf("Error\n");
-        return 98;
-    }
+/**
+*conf_number - confirms if number
+*@num: pointer to value
+*return 0 or 1;
+*/
 
-    if (!_isnum(argv[1]) || !_isnum(argv[2])) {
-        printf("Error\n");
-        return 98;
-    }
+int conf_number(char *num)
+{
+	int iter = 0;
+	
+	do {
+		if (!conf_digit(num[iter]))
+			return (0);
+		iter++;
+	} while (num[iter]);
+	
+	return (1);
+}
 
-    result = _mul(argv[1], argv[2]);
-    printf("%d\n", result);
-    return 0;
+/**
+* conv_to_int - string converterto an integer
+*
+* @s: takes for string passed
+*
+* Return: string
+*/
+/*int conv_to_int(char *s)
+{
+int x =0, y = 0, z = 0, ext = 0, result = 0;
+int sign;
+while (s[ext] != '0')
+ext++;
+for (; x < ext && y == 0; x++)
+{
+if (s[x] == '-')
+z++;
+if (s[x] >= '0' && s[x] <= '9')
+{
+sign = s[x] - '0';
+if (z % 2)
+sign = -sign;
+result = result * 10 + sign;
+y = 1;
+if (s[x + 1] < 48 || s[x + 1] > 57)
+break;
+y = 0;
+
+}
+}
+
+if (y == 0)
+return (0);
+return (result);
+}
+*/
+/**
+*mul -  multiplies two positive numbers
+*@num1: pointer to first positive number
+*@num2: pointer to second positive number
+*Return: product
+*/
+
+int mul(char *num1, char *num2)
+{
+	int y = atoi(num1);
+	int x = atoi(num2);
+	int product = x * y;
+	
+	return(product);
 }
