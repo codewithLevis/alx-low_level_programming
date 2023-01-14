7a827706556ebe0a824229dfca1d8eca43b7c141
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-15 00:37:49 +0300

    Create 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* *_realloc - reallocates a memory block using malloc and free
+* @ptr: a pointer to the memory previously allocated with a call to malloc
+* @old_size:  the size, in bytes, of the allocated space for ptr
+* @new_size:  the new size, in bytes of the new memory block
+*
+* Return: pointer new memory
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+char *str1, *str2;
+unsigned int n;
+
+if (new_size == old_size)
+return (ptr);
+
+if (new_size == 0 && ptr)
+{
+free(ptr);
+return (NULL);
+}
+
+if (!ptr)
+return (malloc(new_size));
+
+str1 = malloc(new_size);
+if (!str1)
+return (NULL);
+
+str2 = ptr;
+
+if (new_size < old_size)
+{
+n = 0;
+do {
+str1[n] = str2[n];
+n++;
+} while (n < new_size);
+}
+
+if (new_size > old_size)
+{
+n = 0;
+do {
+str1[n] = str2[n];
+n++;
+} while (n < old_size);
+}
+
+free(ptr);
+return (str1);
+}
