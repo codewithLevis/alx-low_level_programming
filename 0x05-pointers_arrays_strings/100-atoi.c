5d4e1da0bbe59afcfe4027c0389268d90ea7b688
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-15 15:40:19 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,29 +9,34 @@
 */
 int _atoi(char *s)
 {
-int x =0, y = 0, z = 0, ext = 0, result = 0;
-int sign;
-while (s[ext] != '0')
-ext++;
-for (; x < ext && y == 0; x++)
+int counter1, counter2, Flag, len;
+int number, digit;
+
+counter1 = counter2 = digit = number = len = 0;
+
+for (len = 0; s[len] != '\0'; len++);
+
+do{
+if (s[counter2] == '-')
+counter1++;
+if (s[counter2] >= '0' && s[counter2] <= '9')
 {
-if (s[x] == '-')
-z++;
-if (s[x] >= '0' && s[x] <= '9')
-{
-sign = s[x] - '0';
-if (z % 2)
-sign = -sign;
-result = result * 10 + sign;
-y = 1;
-if (s[x + 1] < 48 || s[x + 1] > 57)
+digit = s[counter2] - '0';
+if (counter1 % 2)
+digit = -digit;
+number = number * 10 + digit;
+Flag = 1;
+if (s[counter2 + 1] < '0' || s[counter2 + 1] > '9')
 break;
-y = 0;
 
-}
+Flag = 0;
 }
 
-if (y == 0)
+counter2++;
+} while (counter2 < len && Flag == 0);
+
+if (Flag == 0)
 return (0);
-return (result);
+
+return (number);
 }
