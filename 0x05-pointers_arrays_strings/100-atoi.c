2518569b45ea47d6409374f2d0c665fa128f9c2c
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-15 16:26:41 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,55 +1,55 @@
-  #include "main.h"
+#include "main.h"
 
-  /**
-  *stringlen - finds len of string
-  *@s: string pointer
-  *Return: string len
-  */
-  int stringlen(char *s)
-  {
-  int i;
-  for (i = 0; s[i] != '\0';)
-  i++;
-  return (i);
-  }
+/**
+*stringlen - finds len of string
+*@s: string pointer
+*Return: string len
+*/
+int stringlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0';)
+i++;
+return (i);
+}
 
-  /**
-  * _atoi - string converterto an integer
-  *
-  * @s: takes for string passed
-  *
-  * Return: string
-  */
-  int _atoi(char *s)
-  {
-  int counter1, counter2, Flag, len;
-  int number, digit;
+/**
+* _atoi - string converterto an integer
+*
+* @s: takes for string passed
+*
+* Return: string
+*/
+int _atoi(char *s)
+{
+int counter1, counter2, , len;
+int number, digit;
+Flag = 0;
+counter2 = digit = number = 0;
+counter1 = 0;
+len = stringlen(s);
 
-  counter2 = digit = number = 0;
-  counter1 = 0;
-  len = stringlen(s);
+do{
+if (s[counter2] == '-')
+++counter1;
+if (s[counter2] >= '0' && s[counter2] <= '9')
+{
+digit = s[counter2] - '0';
+if (counter1 % 2)
+digit = -digit;
+number = number * 10 + digit;
+Flag = 1;
+if (s[counter2 + 1] < '0' || s[counter2 + 1] > '9')
+break;
 
-  do{
-  if (s[counter2] == '-')
-  ++counter1;
-  if (s[counter2] >= '0' && s[counter2] <= '9')
-  {
-  digit = s[counter2] - '0';
-  if (counter1 % 2)
-  digit = -digit;
-  number = number * 10 + digit;
-  Flag = 1;
-  if (s[counter2 + 1] < '0' || s[counter2 + 1] > '9')
-  break;
+Flag = 0;
+}
 
-  Flag = 0;
-  }
+counter2++;
+} while (counter2 < len && Flag == 0);
 
-  counter2++;
-  } while (counter2 < len && Flag == 0);
+if (Flag == 0)
+return (0);
 
-  if (Flag == 0)
-  return (0);
-
-  return (number);
-  }
+return (number);
+}
