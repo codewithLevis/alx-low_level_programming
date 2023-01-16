6a8b7a1b5168b0c44b7bda8b8673187511938a33
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-16 21:43:50 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,15 +1,27 @@
 #include "main.h"
-
+/**
+*_strcmp - compares two strings
+*@s1: pointer to first string
+*@s2: pointer to second string
+*Return: value of comparison
+*Differnce of ASCII code at the differenting letters
+*/
 int _strcmp(char *s1, char *s2)
 {
-int m, compstr;
+int m;
+int cmp;
 
-m = 0;
-
-do {
-m++;
-} while (s1[m] == s2[m] && s1[m] != '\0');
-
-compstr = s1[m] - s2[m];
-return (compstr);
+for (m = 0;s1[m] != '\0' && s2[m] != '\0'; m++)
+{
+/*If strings are equal*/
+if (s1[m] == s2[m])
+cmp = 0;
+/*search for the point the two strings differ*/
+if (s1[m] != s2[m])
+{
+cmp = s1[m] - s2[m];
+break;
+}
+}
+return (cmp);
 }
