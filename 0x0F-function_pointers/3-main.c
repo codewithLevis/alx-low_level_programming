b43536c911765adb511039f84c3d97cf952ae23c
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-20 02:12:19 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,40 +3,40 @@
 #include "3-calc.h"
 
 /**
- * main - execution of program
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0(success) or exits(Failure)
- */
+* main - execution of program
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: 0(success) or exits(Failure)
+*/
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, final_result;
-	int (*f)(int, int);
+int num1, num2, final_result;
+int (*f)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	f = get_op_func(argv[2]);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+f = get_op_func(argv[2]);
 
-	if (f == NULL || (argv[2][1] != '\0'))
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
-	{
-		printf("Error\n");
-		exit(100);
-	}
-  
-  final_result = f(n1, n2);
-	printf("%d\n", final_result);
+if (f == NULL || (argv[2][1] != '\0'))
+{
+printf("Error\n");
+exit(99);
+}
+if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+{
+printf("Error\n");
+exit(100);
+}
 
-	return (0);
+final_result = f(num1, num2);
+printf("%d\n", final_result);
+
+return (0);
 }
