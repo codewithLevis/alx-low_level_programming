6af7887343b45c028c1273f94f76a9a83d22a267
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-20 02:00:08 +0300

    Create 3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - add
+* @a: first number parameter
+ * @b: second number parameter
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract
+ * @a: first number parameter
+ * @b: second number parameter
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply
+ * @a: first number parameter
+ * @b: second number parameter
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - gets quotient
+ * @a: first number parameter
+ * @b: second number parameter
+ * Return: quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - gets modulus
+ * @a: first number parameter
+ * @b: second number parameter
+ * Return: modulus
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
