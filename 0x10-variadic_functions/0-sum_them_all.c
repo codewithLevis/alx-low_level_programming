6e3589340d0aa34eb8a855c0ea7fb9c163e2b28f
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-22 01:06:32 +0300

    Create 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+*sum_them_all - returns sum of all its parameters
+*
+*@n: first parameter
+*
+*Return: NULL or total
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int m;
+	int total = 0;
+
+	va_list value;
+	if (n != 0)
+	{
+		va_start(value, n);
+		m = 0;
+
+		while (m < n)
+		{
+			total += va_arg(value, int);
+			m++;
+		}
+		return (total);
+	}
+
+	return (0);
+}
