d5810f9211fb351f3f3ee0a51859b475709179c0
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-22 20:49:21 +0300

    Create 100-main_opcodes.c

---
+++ 0x0E-structures_typedef/100-main_opcodes.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - outputs opcodes
+*
+*@argc: number of arguments
+*@argv: array of arguments
+*
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	char *ptr;
+	int m, byte;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	byte = atoi(argv[1]);
+
+	if (byte < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	ptr = (char *)main;
+	m = 0;
+
+	while (m < byte)
+	{
+		printf("%02hhx ", ptr[m]);
+
+		if (m == byte - 1)
+		{
+			printf("%02hhx\n", ptr[m]);
+			break;
+		}
+	}
+	return (0);
+}
