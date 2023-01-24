91cdd1809220f68d5d5769ec2a4940f0dd67d015
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-25 02:57:52 +0300

    Create 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+/**
+*_strdup - returns a pointer to a newly string
+*which is a duplicate of the string str
+*@str: string
+*Return: sucess or null
+*/
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *s;
+
+	for(i = 0; str[i] != '\0'; i++);
+
+	s = malloc(i + 1));
+
+	if (s == NULL)
+		return (NULL);
+
+	j = 0;
+
+	while (j < i)
+	{
+	s[j] = str[j];
+	j++;
+	}
+
+	s[j] = '\0';
+	return (s);
+}
