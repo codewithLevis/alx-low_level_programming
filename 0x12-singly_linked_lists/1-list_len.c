69385b0a8777a0deee6d9b9ca04d705581937867
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-31 11:57:12 +0300

    Create 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len -  function returns the number of elements in a linked list_t list.
+ * @h: pointer to the linked list list_t 
+ *
+ * Return: the number of elements in a linked list_t list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t m = 0;
+	const list_t *current = h;
+
+	for (;current != NULL; m++)
+		current = current->next;
+
+	return (m);
+}
