3df1d7ead95ec4f9fd3070476e72e02ee1692f9d
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-31 22:16:17 +0300

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,10 +10,9 @@ void free_list(list_t *head)
 	list_t *current = head;
 	list_t *ptr;
 
-	while (current != NULL)
+	while (!current)
 	{
 		ptr = current->next;
-		free(current->str);
 		free(current);
 		current = ptr;
 	}
