f9a002261f1ef2f0dca76c2be4d8a3920166fe68
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-31 12:23:21 +0300

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void print_before(void) __attribute__ ((constructor));
+
+/**
+ * first -  function that prints
+ *before the main function is executed
+ * Return: void
+ */
+
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
