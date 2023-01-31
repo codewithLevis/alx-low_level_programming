f4d65144a2934b32505d37374c26e7438c50cd2b
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-31 12:07:16 +0300

    Create 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * _strlen - find string's length
+ * @s: string pointer
+ * Return: length of string
+ */
+
+int _strlen(const char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: double pointer to struct list_t list
+ * @str: pointer to string to be added at the beginning of list
+ * Return: list
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *data;
+
+
+	data = malloc(sizeof(list_t));
+	if (data == NULL)
+		return (NULL);
+
+	data->str = strdup(str);
+
+	if (data->str == NULL)
+	{
+	    free(data);
+		return (NULL);
+	}
+	data->len = _strlen(str);
+
+
+	if (head == NULL)
+		data->next = NULL;
+	else
+		data->next = *head;
+
+	*head = data;
+
+	return (data);
+}
