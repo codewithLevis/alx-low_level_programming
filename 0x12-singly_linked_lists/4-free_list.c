b0777ddcf1ff420817989ca9eaa0b321074a9d16
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-01-31 12:36:20 +0300

    Create 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list -  frees a list_t list
+ * @head: pointer to list list_t
+ *Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *ptr;
+
+	while (current)
+	{
+		ptr = current->next;
+		free(current->str);
+		free(current);
+		current = current;
+	}
+	head = NULL;
+}
