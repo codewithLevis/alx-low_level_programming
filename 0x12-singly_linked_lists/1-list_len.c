18bc1a8168fe306958134cff69b1a5c5c86b7694
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-02-01 03:39:36 +0300

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 
 /**
  * list_len -  function returns the number of elements in a linked list_t list.
- * @h: pointer to the linked list list_t 
+ * @h: pointer to the linked list list_t
  *
  * Return: the number of elements in a linked list_t list
  */
@@ -12,7 +12,7 @@ size_t list_len(const list_t *h)
 	size_t m = 0;
 	const list_t *current = h;
 
-	for (;current != NULL; m++)
+	for (; current != NULL; m++)
 		current = current->next;
 
 	return (m);
