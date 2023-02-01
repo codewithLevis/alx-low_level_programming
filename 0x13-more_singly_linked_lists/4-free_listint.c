cc1307d0dad89d8bda30a9b5919eaa1cebe32748
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-02-02 01:19:57 +0300

    Create 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+*free_listint - a function that frees a listint_t list
+*@head: pointer to list to be freed
+*Return: void
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head;
+		head = ptr->next;
+		free(ptr);
+	}
+}
