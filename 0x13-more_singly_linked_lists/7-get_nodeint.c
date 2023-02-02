95ada9091f5202b8561b247174085e4555e85215
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-02-02 03:17:45 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,23 +1,6 @@
 #include "lists.h"
 
 /**
-*listint_len - returns the number of elements in a linked listint_t list.
-*@h: pointer to tempelate of linked list
-*Return: the number of elements in a linked listint_t list.
-*/
-
-size_t listint_len(const listint_t *h)
-{
-size_t m = 0;
-const listint_t *ptr = h;
-
-for (; ptr != NULL; m++)
-ptr = ptr->next;
-
-return (m);
-}
-
-/**
 *get_nodeint_at_index - gets nth node of a listint_t linked list
 *
 *@head: list
@@ -28,24 +11,16 @@ return (m);
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-listint_t *ptr, *nth_node;
-unsigned int m, len = listint_len(head);
+listint_t *nth_node;
+unsigned int m;
 
-nth_node = (listint_t *) malloc(sizeof(listint_t));
-
-if (nth_node == NULL)
-return (NULL);
-
-if (index > len)
-return (NULL);
-
-ptr = head;
-
-for (m = 0; ptr != NULL && m < index - 1; m++)
+nth_node = head;
+for (m = 0; nth_node != NULL; m++)
 {
-ptr = ptr->next;
+if (m == index)
+return (nth_node);
+nth_node = nth_node->next;
 }
-nth_node = ptr->next;
 
 return (nth_node);
 }
