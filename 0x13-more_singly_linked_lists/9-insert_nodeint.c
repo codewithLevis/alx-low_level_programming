463d23cd1865685cbf702ab2b598b44d813121f1
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-02-02 23:57:48 +0300

    Create 9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+*insert_nodeint_at_index -  inserts a new node at a given position
+*@head: pointer to a list template
+*@idx: is the index of the list where the new node should be added
+*@n: integer to be added at a given position
+*Return: the address of the new node, or NULL if it failed
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *ptr, *nth_position;
+
+	nth_position = (listint_t *) malloc(sizeof(listint_t));
+
+	if (nth_position == NULL)
+		return (NULL);
+
+	(*nth_position).n = n;
+	(*nth_position).next = NULL;
+
+	if (idx == 0)
+	{
+		nth_position->next = (*head);
+		*head = nth_position;
+		return (nth_position);
+	}
+
+	ptr = *head;
+
+	for (int i = 0; i< (idx - 1) && ptr != NULL; i++, ptr = ptr->next)
+	{
+		if (ptr->next == NULL)
+		{
+			free(nth_position);
+			return(NULL);
+		}
+	}
+
+	(*nth_position).next = ptr->next;
+	ptr->next = nth_position;
+
+	return (nth_position);
+}
