a00c5704ebf438f56071a5dae8f9b74f4973f6ee
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-02-03 00:03:38 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,6 +10,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	unsigned int i;
 	listint_t *ptr, *nth_position;
 
 	nth_position = (listint_t *) malloc(sizeof(listint_t));
@@ -29,12 +30,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	ptr = *head;
 
-	for (int i = 0; i< (idx - 1) && ptr != NULL; i++, ptr = ptr->next)
+	for (i = 0; i < (idx - 1) && ptr != NULL; i++, ptr = ptr->next)
 	{
 		if (ptr->next == NULL)
 		{
 			free(nth_position);
-			return(NULL);
+			return (NULL);
 		}
 	}
 
