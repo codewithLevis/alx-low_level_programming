e18a65f616926992965d31503d922e78da9ed080
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-02-03 17:18:53 +0300

    Create 103-find_loop.c

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+*find_listint_loop - finds the loop in a linked list
+*@head: pointer to alist
+*Return: address of the node where the loop starts, or NULL if there is no loop
+*/
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *search, *locate;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	locate = head->next;
+	hare = (head->next)->next;
+
+	for (; search != NULL; search = (search->next)->next;)
+	{
+		if (locate == search)
+		{
+			locate = head;
+
+			while (locate != search)
+			{
+				locate = locate->next;
+				search = search->next;
+			}
+
+			return (locate);
+		}
+
+		locate = locate->next;
+	}
+
+	return (NULL);
+}
