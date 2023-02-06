3077eb2349e5e7cc2eb30e90b204075ffcd56fe0
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-02-07 02:16:34 +0300

    Create 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*flip_bits - function that lips bits
+*@n: integer one
+*@b: integer two
+*Return: the number of bits you would need to flip
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int xor = n ^ m;
+	unsigned int i = 0;
+
+	while (xor > 0)
+	{
+		i += (xor & 1);
+		xor >>= 1;
+	}
+
+	return (i);
+}
