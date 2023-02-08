2fb24b4228fc9869cc40c94133a1e8cdb7773725
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-02-09 02:47:41 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,42 +6,40 @@
 *@letters: the number of letters it should read and print
 *Return: the actual number of letters it could read and print
 */
- 
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        int fp;
-	ssize_t counter, check;
-        char *info;
+int fp;
+ssize_t counter, check;
+char *info;
 
-	if (filename == NULL)
-		return (0);
+if (filename == NULL)
+return (0);
 
-	info = malloc(letters);
+info = malloc(letters);
 
-	if (info == NULL)
-		  return 0;
+if (info == NULL)
+return 0;
 
-        fp = open(filename, O_RDONLY);
+fp = open(filename, O_RDONLY);
 
-	if (fp == -1)
-	{
-		free(info);
-		return (0);
-	}
+if (fp == -1)
+{
+free(info);
+return (0);
+}
 
-	counter = read(fp, info, letters);
-	check = write(1, info, counter);
+counter = read(fp, info, letters);
+check = write(1, info, counter);
 
-	if (counter == -1 || check == -1 || check != counter)
-	{
-		free(info);
-		return (0);
-	}
+if (counter == -1 || check == -1 || check != counter)
+{
+free(info);
+return (0);
+}
 
-	free(info);
-	close(fp);
-	
+free(info);
+close(fp);
 
-	return (check);
-
+return (check);
 }
