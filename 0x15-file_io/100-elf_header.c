3a37221526fa90ccaa8ad84677c6d1bce824d59b
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-02-15 01:40:06 +0300

    Create 100-elf_header.c

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - a program that displays the information contained 
+ *@ac: Argument count
+ * @av: arguments
+ * Return: 0
+ */
+
+int main(char argc, int *argv[])
+{
+	return (0);
+}
