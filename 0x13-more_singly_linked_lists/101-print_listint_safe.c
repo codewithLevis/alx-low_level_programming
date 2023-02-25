d91c393acd4633038c4aff7caa4f7605bbe2c9ae
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-02-25 18:46:24 +0300

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 
+void free_listp(listp_t **head);
+
 /**
 *print_listint_safe - function that prints a listint_t linked list
 *@head: pointer to list
@@ -11,25 +13,62 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	long int difference;
+	size_t node_count = 0;
+	listp_t *ptr_head, *new_node, *add_node;
 
-	do {
-		difference = head - head->next;
-		count++;
+	ptr_head = NULL;
+
+	for (; head != NULL; head = head->next)
+	{
+		new_node = malloc(sizeof(listp_t));
+
+		if (new_node == NULL)
+			exit(98);
+
+		new_node->p = (void *)head;
+		new_node->next = ptr_head;
+		ptr_head = new_node;
+
+		add_node = ptr_head;
+
+		while (add_node->next != NULL)
+		{
+			add_node = add_node->next;
+			if (head == add_node->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&ptr_head);
+				return (node_count);
+			}
+		}
+
 		printf("[%p] %d\n", (void *)head, head->n);
+		node_count++;
+	}
 
-		if (difference > 0)
-		{
-			head = head->next;
-		}
-		else
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
-		}
-	} while (head != NULL);
-
-	return (count);
+	free_listp(&ptr_head);
+	return (node_count);
 }
 
+/**
+*free_listp - free linked list
+*@head: pointer to a linked list
+*Return: void
+*/
+void free_listp(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head)
+	{
+		curr = *head;
+
+		for (; curr != NULL; curr = curr->next)
+		{
+			temp = curr;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
