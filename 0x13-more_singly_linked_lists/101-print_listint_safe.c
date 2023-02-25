23a6346c455ec1507c75ea58dfda6afd728ef64c
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-02-25 20:49:30 +0300

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * loop_listint_l - determines how many distinct nodes 
- * there are in a looped listint_t linked list.
- * @head: listint_t pointer
- * Return: If not, the number of distinct nodes in the list
- */
+* looped_listint_len - determines how many distinct nodes 
+* there are in a looped listint_t linked list.
+* @head: listint_t pointer
+* Return: If not, the number of distinct nodes in the list
+*/
 
 size_t looped_listint_len(const listint_t *head)
 {
