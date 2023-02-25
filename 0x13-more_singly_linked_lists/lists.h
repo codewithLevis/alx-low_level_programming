7f643ee78327a16d8f63379fc12462d3a3184a1e
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-02-25 18:50:38 +0300

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,17 @@ int n;
 struct listint_s *next;
 } listint_t;
 
+/**
+ * struct listp_s - list
+ * @p: node pointers
+ * @next: pointers to next node
+ */
+typedef struct listp_s
+{
+void *p;
+struct listp_s *next;
+} listp_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
