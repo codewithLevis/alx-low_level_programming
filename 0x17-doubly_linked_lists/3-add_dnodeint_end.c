7a1f0809f941982d0a1315a771027379c1d6c92e
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-17 05:22:02 +0300

    Create 3-add_dnodeint_end.c

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add node at end of list
+ * @head: head of list
+ * @n: element
+ * Return: list
+*/
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+        dlistint_t *trav, *new;
+        new = malloc(sizeof(dlistint_t));
+
+        new->n = n;
+        new->next = NULL;
+
+        if (*head == NULL)
+        {
+                new->prev = NULL;
+                *head = new;
+                return (*head);
+        }
+
+        trav = *head;
+        while (trav->next != NULL)
+                trav = trav->next;
+        
+        trav->next = new;
+        new->prev = trav;
+
+        return (*head);
+}
