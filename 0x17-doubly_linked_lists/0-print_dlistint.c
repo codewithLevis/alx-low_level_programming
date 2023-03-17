8ee5be1156839cd1fa28ce31a23dbe06ebaf89d4
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-17 05:17:07 +0300

    Create 0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints list
+ * @h: head of list
+ * Return: node
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+        dlistint_t *trav = (dlistint_t *) h;
+        size_t i = 0;
+
+        for (; trav != NULL; i++)
+        {
+                printf("%d\n", trav->n);
+                trav = trav->next;
+        }
+                
+        
+        return (i);
+}
