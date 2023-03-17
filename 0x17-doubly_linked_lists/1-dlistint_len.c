caf90eb228d91dc51ecd2d968f2b263fc3598ada
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-17 05:18:32 +0300

    Create 1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - finds list len
+ * @h: head of list
+ * Return: node count
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+        dlistint_t *trav = (dlistint_t *) h;
+        size_t i = 0;
+
+        for (; trav != NULL; i++)
+                trav = trav->next;                
+        
+        return (i);
+}
