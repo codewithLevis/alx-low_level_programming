2718bef70d2a71bfd8fd9c181ead118dc444c873
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-17 05:20:32 +0300

    Create 2-add_dnodeint.c

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add node in front of list
+ * @head: head of list
+ * @n: element
+ * Return: list
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+        dlistint_t *new = malloc(sizeof(dlistint_t));
+        new->n = n;
+        new->prev = NULL;
+        new->next = (*head);
+        (*head) = new;
+
+        return (*head);
+}
