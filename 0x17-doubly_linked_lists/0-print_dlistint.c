5064a96ce37dec5f00518f568549ff22e40effcb
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-17 05:29:19 +0300

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,15 +7,14 @@
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-        dlistint_t *trav = (dlistint_t *) h;
-        size_t i = 0;
+	dlistint_t *trav = (dlistint_t *) h;
+	size_t i = 0;
 
-        for (; trav != NULL; i++)
-        {
-                printf("%d\n", trav->n);
-                trav = trav->next;
-        }
-                
-        
-        return (i);
+	for (; trav != NULL; i++)
+	{
+		printf("%d\n", trav->n);
+		trav = trav->next;
+	}
+
+	return (i);
 }
