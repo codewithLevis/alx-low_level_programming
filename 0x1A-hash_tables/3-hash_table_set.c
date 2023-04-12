d373d349e9809ab2e25aad74fa9f6a14bc18f8c7
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-04-13 01:20:07 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,17 +13,13 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int idx;
-	char *value_dup;
 	hash_node_t *new, *temp;
 
-	if (!ht || !key|| strlen(key) == 0 || !value)
+	if (!ht || !key || strlen(key) == 0 || !value)
 		return (0);
-
-	value_dup = strdup(value);
 
 	if (value_dup == NULL)
 		return (0);
-
 	idx = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[idx];
 
@@ -32,30 +28,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(temp->key, key) == 0)
 		{
 			free(temp->value);
-			temp->value = value_dup;
+			temp->value = strdup(value);;
 			return (1);
 		}
 		temp = temp->next;
 	}
-
 	new = malloc(sizeof(hash_node_t));
 
 	if (new == NULL)
-	{
-		free(value_dup);
 		return (0);
-	}
 
 	new->key = strdup(key);
 
 	if (new->key == NULL)
 	{
-		free(value_dup);
 		free(new);
 		return (0);
 	}
-
-	new->value = value_dup;
+	new->value = strdup(value);
 	new->next = ht->array[idx];
 	ht->array[idx] = new;
 	return (1);
